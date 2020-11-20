69728ef24e67d8129d23506b9e96d9dea799692d
AmolMhatre <amolmhatre11@gmail.com>
2020-11-20 11:40:30 +0530

    Custom action bar

--- app/src/main/java/com/amol/waterbill/MainActivity.java
+++ app/src/main/java/com/amol/waterbill/MainActivity.java
@@ -1,5 +1,6 @@
 package com.amol.waterbill;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -11,22 +12,48 @@ import android.widget.EditText;
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
 
-    EditText etConnectionNumber;
-
+    private static EditText etConnectionNumber;
+    private static Button btnGetConnection,btnRegisterConnection;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        this.getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setDisplayShowCustomEnabled(true);
+        getSupportActionBar().setCustomView(R.layout.action_bar);
+        //getSupportActionBar().setElevation(0);
+
         etConnectionNumber =  (EditText) findViewById(R.id.etConnectionNumber);
+        btnGetConnection = (Button) findViewById(R.id.btnGetConnection);
+        btnRegisterConnection = (Button) findViewById(R.id.btnRegisterConnection);
+
+        btnGetConnection.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                searchConnection();
+            }
+        });
+
+        btnRegisterConnection.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                registerConnection();
+            }
+        });
     }
 
     /** Called when the user taps button */
-    public void searchConnection(View view) {
+    public void searchConnection() {
         Intent intent = new Intent(this, CustomerDetail.class);
         String connectionNumber = etConnectionNumber.getText().toString();
         intent.putExtra("connection", connectionNumber);
+        startActivity(intent);
+    }
+
+    public void registerConnection() {
+        Intent intent = new Intent(this, RegisterUser.class);
         startActivity(intent);
     }
 }
