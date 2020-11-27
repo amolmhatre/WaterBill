21b216debc88a3c72df00953a9c31f43a7c991dd
AmolMhatre <amolmhatre11@gmail.com>
2020-11-27 13:02:54 +0530

    User Model

---
+++ app/src/main/java/com/amol/waterbill/model/UserModel.java
@@ -0,0 +1,146 @@
+package com.amol.waterbill.model;
+
+/**
+ * Created by amolmhatre on 11/23/20
+ */
+
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class UserModel {
+
+    @SerializedName("bill_no")
+    @Expose
+    private String billNo;
+    @SerializedName("bill_cycle")
+    @Expose
+    private String billCycle;
+    @SerializedName("unit")
+    @Expose
+    private String unit;
+    @SerializedName("unit_price")
+    @Expose
+    private String unitPrice;
+    @SerializedName("amount")
+    @Expose
+    private String amount;
+    @SerializedName("current_reading")
+    @Expose
+    private String currentReading;
+    @SerializedName("user_id")
+    @Expose
+    private String userId;
+    @SerializedName("status")
+    @Expose
+    private String status;
+    @SerializedName("name")
+    @Expose
+    private String name;
+    @SerializedName("mobile_no")
+    @Expose
+    private String mobileNo;
+    @SerializedName("email")
+    @Expose
+    private String email;
+    @SerializedName("address")
+    @Expose
+    private String address;
+
+    public String getBillNo() {
+        return billNo;
+    }
+
+    public void setBillNo(String billNo) {
+        this.billNo = billNo;
+    }
+
+    public String getBillCycle() {
+        return billCycle;
+    }
+
+    public void setBillCycle(String billCycle) {
+        this.billCycle = billCycle;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+
+    public String getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(String unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    public String getAmount() {
+        return amount;
+    }
+
+    public void setAmount(String amount) {
+        this.amount = amount;
+    }
+
+    public String getCurrentReading() {
+        return currentReading;
+    }
+
+    public void setCurrentReading(String currentReading) {
+        this.currentReading = currentReading;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMobileNo() {
+        return mobileNo;
+    }
+
+    public void setMobileNo(String mobileNo) {
+        this.mobileNo = mobileNo;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+}
